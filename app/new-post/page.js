bdd179b8ba89eedd906adbfcada77379dbac0d5f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-07-24 14:25:20 +0800

    Add dynamic and static metada to all pages

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,6 +1,11 @@
 import { createPost } from '@/actions/posts';
 import PostForm from '@/components/post-form';
 
+export const metadata = {
+	title: 'Create new post',
+	description: 'Create and share amazing posts.',
+};
+
 export default function NewPostPage() {
 	return <PostForm action={createPost} />;
 }
